25e838a3dd7a2fa41d87c1a450d9ab7fd1f35480
Daniel Vu <da949990@ucf.edu>
2026-09-10 12:21:29 -0400

    Update constraint file.

--- lab_1/lab_1.runs/impl_1/rundef.js
+++ lab_1/lab_1.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log lab_1.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source lab_1.tcl -notrace" );
 
