d3c900539df983a94a929a3b118a9b5b2f5c28b2
Daniel Winkelmann <da133662@ucf.edu>
2026-09-17 11:54:45 -0400

    Create lab 2.

---
+++ project_1/project_1.srcs/sources_1/new/vga_color_changer.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/17/2026 11:53:34 AM
+// Design Name: 
+// Module Name: vga_color_changer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vga_color_changer(
+
+    );
+endmodule
